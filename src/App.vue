b9d5f1c811832bd1dcb2282fd2bd713dce96d3ef
Bryant-Wang24 <485969746@qq.com>
2021-02-20 19:58:10 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <router-view />
+  <router-view/>
   <Frank/>
 </template>
 
 <script lang="ts">
-import {ref,provide} from 'vue'
+import {ref, provide} from 'vue';
+import {router} from './router';
+
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true);
-    provide("menuVisible", menuVisible); // set
+    provide('menuVisible', menuVisible); // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   },
 };
 </script>
