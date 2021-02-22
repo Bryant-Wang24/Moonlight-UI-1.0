daecb9b07a4e8590cf55756c1c476d06f6a6262c
Bryant-Wang24 <485969746@qq.com>
2021-02-22 19:50:33 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
-      <div class="gulu-tabs-nav-indicator"></div>
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import {computed} from 'vue';
+import {computed, ref,onMounted,onUpdated} from 'vue';
   export default {
   props:{
     selected: {
@@ -19,6 +20,28 @@ import {computed} from 'vue';
     }
   },
     setup(props,context){
+    const navItems = ref<HTMLDivElement[]>([])
+      const indicator = ref < HTMLDivElement > (null)
+      const container = ref < HTMLDivElement > (null)
+      const x = () => {
+        const divs = navItems.value
+        const result = divs.filter(div => div.classList.contains('selected'))[0]
+        console.log(result)
+        const {
+          width
+        } = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = result.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      }
+      onMounted(x)
+      onUpdated(x)
       const defaults = context.slots.default()
       defaults.forEach((tag)=>{
         if (tag.type!==Tab){
@@ -26,7 +49,6 @@ import {computed} from 'vue';
         }
       })
       const current = computed(()=>{
-        console.log('重新 return');
         return tag.defaults.filter((tag)=>{
           return tag.props.title===props.selected
         })[0]
@@ -38,7 +60,7 @@ import {computed} from 'vue';
         context.emit('update:selected',title)
       }
       return{
-        defaults,titles,current,select
+        defaults,titles,current,select,navItems,indicator,container
       }
     }
   }
@@ -72,6 +94,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
