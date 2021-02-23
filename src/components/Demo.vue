f02e85d181ab5cdde41ec60d2e8c112d99842a74
Bryant-Wang24 <485969746@qq.com>
2021-02-23 23:08:10 +0800

    显示隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="toggleCode">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-html" v-html="html" />
@@ -21,10 +22,10 @@ import 'prismjs/themes/prism.css'
 import {
   computed,
   ref
-} from 'vue';
+} from 'vue'
 const Prism = (window as any).Prism
 export default {
-  components:{
+  components: {
     Button
   },
   props: {
@@ -34,13 +35,15 @@ export default {
     const html = computed(() => {
       return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
     })
-    const toggleCode = () => codeVisible.value = !codeVisible.value
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
     const codeVisible = ref(false)
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode
+      showCode,
+      hideCode
     }
   }
 }
