844db1f1ba99eec77c592286e333959433b9b5cc
Bryant-Wang24 <485969746@qq.com>
2021-02-23 23:48:37 +0800

    修复

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -39,6 +39,8 @@ import { ref,
           } = selectedItem.value.getBoundingClientRect()
           const left = left2 - left1
           indicator.value.style.left = left + 'px'
+        }, {
+          flush: 'post'
         })
       })
       const defaults = context.slots.default()
