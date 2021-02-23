c3811a8e6c70e4bb8ad2bebba90756abd5234660
Bryant-Wang24 <485969746@qq.com>
2021-02-23 23:26:36 +0800

    优化官网

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -54,7 +54,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+$aside-index : 10;
 .layout {
   display: flex;
   flex-direction: column;
@@ -97,6 +97,7 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
+  z-index: $aside-index;
 
   > h2 {
     margin-bottom: 4px;
