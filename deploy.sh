6c6244f67460a388f779bc145ab29f2d1955689a
Bryant-Wang24 <485969746@qq.com>
2021-07-19 00:01:16 +0800

    modify bug

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:Bryant-Wang24/Moonlight-UI-1.0.git &&
+git remote add origin git@github.com:Bryant-Wang24/moonlight-ui-website &&
 git push -f -u origin master &&
 cd -
 echo https://bryant-wang24.github.io/moonlight-ui-website/index.html
