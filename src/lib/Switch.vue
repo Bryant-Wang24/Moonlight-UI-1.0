9b50ad03beb9a4cb454ed79598e9e2f9939e411a
Bryant-Wang24 <485969746@qq.com>
2021-07-19 21:08:47 +0800

    modify color

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,39 +1,41 @@
 <template>
-  <button class="gulu-switch" @click="toggle" :class="{'gulu-checked':value}">
+  <button
+    class="gulu-switch"
+    @click="toggle"
+    :class="{ 'gulu-checked': value }"
+  >
     <span></span>
   </button>
 </template>
 
 <script>
-import {
-  ref
-} from 'vue'
+import { ref } from "vue";
 export default {
   props: {
     value: Boolean,
   },
   setup(props, context) {
     const toggle = () => {
-      context.emit('update:value', !props.value)
-    }
-    return {
-      toggle
+      context.emit("update:value", !props.value);
     };
-  }
+    return {
+      toggle,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-$h:22px;
-$h2:$h - 4px;
+$h: 22px;
+$h2: $h - 4px;
 .gulu-switch {
   height: $h;
-  width: $h*2;
+  width: $h * 2;
   border: none;
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-  >span {
+  > span {
     position: absolute;
     top: 2px;
     left: 2px;
@@ -44,8 +46,8 @@ $h2:$h - 4px;
     transition: all 250ms;
   }
   &.gulu-checked {
-    background: blue;
-    >span {
+    background: #1890ff;
+    > span {
       left: calc(100% - #{$h2} - 2px);
     }
   }
@@ -53,14 +55,14 @@ $h2:$h - 4px;
     outline: none;
   }
   &:active {
-    >span {
-      width: $h2 + 4px
+    > span {
+      width: $h2 + 4px;
     }
   }
   &.gulu-checked:active {
-    >span {
+    > span {
       width: $h2 + 4px;
-      margin-left: -4px
+      margin-left: -4px;
     }
   }
 }
