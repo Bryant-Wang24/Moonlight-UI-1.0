a392908f894632e3d38605420d10fce0a577e897
Bryant-Wang24 <485969746@qq.com>
2021-12-08 11:47:14 +0800

    popup页面样式完成，打开关闭popup功能完成

--- src/components/Popup1.demo.vue
+++ src/components/Popup1.demo.vue
@@ -3,17 +3,33 @@
 </demo>
 <template>
     <div>
-        <Button>展示弹出层</Button>
+        <Button @click="toggle">展示弹出层</Button>
+        <Popup :visible="x" @update:visible="x=$event">
+          <template v-slot:content>
+            <div>内容1</div>
+            <div>内容2</div>
+            <div>内容3</div>
+          </template>
+<!--          <template v-slot:title>-->
+<!--            <strong>加粗的标题</strong>-->
+<!--          </template>-->
+        </Popup>
     </div>
 </template>
 
-<script>
-import {Button} from "../lib/index"
+<script lang="ts">
+import {Button,Popup} from "../lib/index"
+import {ref} from "vue";
 export default {
-    setup () {
-        
+  components: {Popup,Button},
+  setup () {
+      const x = ref(false)
+        const toggle = ()=>{
+          console.log(x.value)
+          x.value = !x.value
+        }
 
-        return {Button}
+        return {Button,Popup,toggle,x}
     }
 }
 </script>
